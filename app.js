1077bdfa527d9adba6f7114bcb16a777f683defe
Ishan <ishan@mtlbigger.com>
2021-12-23 18:21:03 +0530

    Routing: Added two html pages

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const http = require('http');
-
+const fs = require('fs');
 //creates a server
+
+const home = fs.readFileSync(__dirname+'/home.html','utf8');
+const about = fs.readFileSync(__dirname+'/about.html','utf8');
 const server = http.createServer((req,res)=>{
-    console.log(req.url,req.method,req.headers);
+    const url = req.url;
+    res.setHeader('Content-Type','text/html');
+    if (url === "/"){
+        res.write(`${home}`);
+        
+    }
+    else if(url === "/about"){
+        res.write(`${about}`);
+    }
+    res.end();
+    
+    
+
 });
 
 server.listen(3000);
