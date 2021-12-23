60082722729d4be23043b9cbe4ff8380e1697958
Ishan <ishan@mtlbigger.com>
2021-12-23 19:52:39 +0530

    Assignment-1 Complete

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 const http = require('http');
-
 const requestHandler = require('./routes');
-//creates a server
-
-
 const server = http.createServer(requestHandler);
 
 server.listen(3000);
--- routes.js
+++ routes.js
@@ -1,38 +1,45 @@
 const fs = require('fs');
-const home = fs.readFileSync(__dirname+'/home.html','utf8'); // synchronous
-const about = fs.readFileSync(__dirname+'/about.html','utf8'); // synchronous
 
-const requestHandler = (req,res)=>{
-    const url = req.url;
-    const method = req.method;
-    console.log(method);
-    
-    if (url === "/"){
-        res.setHeader('Content-Type','text/html');
-        res.write(`${home}`);
+const requestHandler = (req,res) =>{
+const url = req.url;
+const method = req.method.toLowerCase();
+
+if (url === "/"){
+    res.setHeader("Content-Type","text/html");
+    fs.readFile(__dirname+'/home.html','utf8',(err,data)=>{
+        if (err){
+            console.error(err);
+        }else{
+            res.write(`${data}`);
+            res.end(); 
+        }
         
-    }
-    else if(url === "/about"){
-        res.setHeader('Content-Type','text/html');
-        res.write(`${about}`);
-    }
-    else if(url === "/message" && method.toLowerCase() === "post"){
-        const body = [];
-        req.on('data',chunk =>{
-            console.log(chunk);
-            body.push(chunk);
-        });
-        req.on('end',()=>{
-            const parsedBody = Buffer.concat(body).toString();
-            console.log(parsedBody);
-        });
+    });  
+}
+else if (url === "/users"){
+    res.setHeader("Content-Type","text/html");
+    fs.readFile(__dirname+'/users.html','utf8',(err,data)=>{
+        if (err){
+            console.error(err);
+        }else{
+            res.write(`${data}`);
+            res.end(); 
+        }
+        
+    });
+    
+}else if (url ==="/create-user" && method === "post"){
+    const body = [];
+    req.on('data',chunk=>{
+        body.push(chunk);
+    })
+    req.on('end',()=>{
+        const parsedBody = Buffer.concat(body).toString();
+        console.log(parsedBody);
         res.statusCode = 302;
         res.setHeader("Location","/");
-    }
-    res.end();
-    
-    
-
-};
-
+        res.end();
+    })
+}
+}
 module.exports = requestHandler;
