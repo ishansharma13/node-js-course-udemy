25f717673b6b89282c43a6e7b12579afe81c3c51
Ishan Sharma <ishansharma1320@gmail.com>
2022-01-24 20:54:58 +0530

    Till Lecture-63, got the basics of express middlewares

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-const http = require('http');
-const requestHandler = require('./routes');
-const server = http.createServer(requestHandler);
+const express = require('express');
 
-server.listen(3000);
+const app = express();
+
+app.use('/add-product',(req,res,next)=>{
+    res.send("<h1> Add-Product Page </h1>");
+})
+app.use('/',(req,res,next)=>{
+    res.send("<h1>Hello from Express!!</h1>");
+})
+
+app.listen(3000);
