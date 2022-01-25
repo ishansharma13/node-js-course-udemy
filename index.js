1dcbbb750df633c1cdaf700c448b405e5497df71
Ishan Sharma <ishansharma1320@gmail.com>
2022-01-25 18:31:46 +0530

    assignment-3 complete: statically serving files

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
+app.use(express.static(path.join(__dirname,'public')));
 
 app.use('/users',(req,res,next)=>{
     console.log("Received request in the first middleware");
-    res.send('<h1>In users</h1>');
+    res.sendFile(path.join(__dirname,'views','users.html'));
 })
 
 app.use('/',(req,res,next)=>{
     console.log("In the second middleware");
-    res.send("<h1>Hello from express!! (assignment-2)");
+    res.sendFile(path.join(__dirname,'views','home.html'));
 })
 
 app.listen(3000);
