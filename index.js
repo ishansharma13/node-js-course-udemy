0644d09e3d1bf7a236e1479925d37e6691a10694
Ishan Sharma <ishansharma1320@gmail.com>
2022-01-25 21:49:43 +0530

    Assignment-4 Complete

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const app = express();
+const users = [];
+app.set('view engine','pug');
+
+app.use(bodyParser.urlencoded({extended:false}));
+
 app.use(express.static(path.join(__dirname,'public')));
 
 app.use('/users',(req,res,next)=>{
+    users.push({name: req.body.name})
+    // console.log(req.body);
     console.log("Received request in the first middleware");
-    res.sendFile(path.join(__dirname,'views','users.html'));
+    // res.sendFile(path.join(__dirname,'views','users.html'));
+    res.render('users',{users: users,pageTitle: 'Home'})
 })
 
-app.use('/',(req,res,next)=>{
+app.get('/',(req,res,next)=>{
     console.log("In the second middleware");
-    res.sendFile(path.join(__dirname,'views','home.html'));
+    // res.sendFile(path.join(__dirname,'views','home.html'));
+    res.render('home',{pageTitle: 'Home'});
 })
 
 app.listen(3000);
